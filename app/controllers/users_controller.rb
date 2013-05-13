1f0b7b62997515b5a78ad3a923c04185c8255ae6
Helen Canzler <helen.t.canzler@gmail.com>
2013-05-13 14:20:18 -0700

    Creation of users feature complete

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+
+    if @user.save
+      flash[:notice] = "User has been created."
+      redirect_to @user
+    else
+      flash
+      # nothing, yet
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PayMyRent::Application.config.secret_token = '58af0ab4f4cbb7790ebff345bdf1fbd4f9f317a0c6735cb99e2d70583b3d3fdea7e6d3ed149e5e95ca8b2bda7eb833c330fd5a05eaccfdc10ff4903326452444'
