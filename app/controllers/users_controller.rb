17600d69f8e793b18d6f3ddd346aa56d30dc95f3
Helen Canzler <helen.t.canzler@gmail.com>
2013-05-13 22:32:41 -0700

    Redirect the users back to the users page if they try going to a project that doesn't exist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_filter :find_user, :only => [:show,
+    :edit,
+    :update,
+    :destroy]
+
   def index
     @users = User.all
   end
@@ -8,11 +13,9 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def create
@@ -28,7 +31,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:notice] = "User has been updated."
       redirect_to @user
@@ -39,9 +41,17 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     flash[:notice] = "User has been deleted."
     redirect_to users_path
   end
+
+  private
+    def find_user
+      @user = User.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The user you were looking for could not be found."
+      redirect_to users_path
+    end
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe UsersController do
-
+  it "displays an error for a missing user" do
+    get :show, :id => "not-here"
+    response.should redirect_to(users_path)
+    message = "The user you were looking for could not be found."
+    flash[:alert].should == message
+  end
 end
