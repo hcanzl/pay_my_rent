97069188e17c28129c63a4bc8cf299501b5f5b54
Helen Canzler <helen.t.canzler@gmail.com>
2013-05-16 19:42:16 -0700

    Updated validation for Amount

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,9 +2,6 @@ class Payment < ActiveRecord::Base
   belongs_to :user
   attr_accessible :amount, :payment_date
 
-  validates :amount, :presence => true
-  #TODO: Add validation for amount>0
-
+  validates :amount, :numericality => { :greater_than => 0 }
   validates :payment_date, :presence => true
-
 end
--- spec/integration/creating_payments_spec.rb
+++ spec/integration/creating_payments_spec.rb
@@ -15,19 +15,12 @@ feature "Creating Payments" do
     page.should have_content("Payment has been created.")
   end
 
-  scenario "Creating a payment without valid attributes fails" do
+  scenario "Amount must be greater than 0" do
+    fill_in "Payment date", :with => "3/5/2012"
+    fill_in "Amount", :with => "-10"
     click_button "Create Payment"
     page.should have_content("Payment has not been created.")
-    page.should have_content("Amount can't be blank")
-    page.should have_content("Payment date can't be blank")
+    page.should have_content("Amount must be greater than 0")
   end
-
-  # scenario "Amount must be greater than 0" do
-  #   fill_in "Payment date", :with => "3/5/2012"
-  #   fill_in "Amount", :with => "0"
-  #   click_button "Create Payment"
-  #   page.should have_content("Payment has not been created.")
-  #   page.should have_content("Amount is less than or equal to 0")
-  # end
 
 end
